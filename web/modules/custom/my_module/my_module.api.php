f6b06dfed9141227407143dbcdb20a0a18f32f80
utkarshInnoraft73 <utkarsh.singh@innoraft.com>
2024-07-26 14:26:22 +0530

    FT2-254: Create a custom hook. - This custom hook count how many type the a particular node in opened.

---
+++ web/modules/custom/my_module/my_module.api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * This is file comments.
+ */
+
+use Drupal\node\NodeInterface;
+
+/**
+ * Method hook_count_node_view.
+ *
+ * @param \Drupal\node\NodeInterface $node
+ *   [Explicite description.
+ */
+function hook_count_node_view(NodeInterface $node): void {
+  // Define the custom logic in the hook.
+}
