5703f0a5c804dbe0ade9031c21eeb4c256ad2b9b
utkarshInnoraft73 <utkarsh.singh@innoraft.com>
2024-08-30 16:15:52 +0530

    FT2-253: Update the code add dependency injection.

--- web/modules/custom/hello_world/src/Controller/ShowTextController.php
+++ web/modules/custom/hello_world/src/Controller/ShowTextController.php
@@ -3,26 +3,56 @@
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Class ShowTextController.
+ * To show the hello text.
  */
 class ShowTextController extends ControllerBase {
 
   /**
-   * Method showText.
+   * Protect currentUser.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Method __construct.
+   *
+   * @param Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current logged in user.
+   */
+  public function __construct(AccountProxyInterface $current_user) {
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * Method create to inject the dependency.
+   *
+   * @param Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   Container that current_user service.
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Method showText to show the custom text.
    *
    * @return array
    *   Text to show the "Hello [User Name]".
    */
   public function showText() : array {
     $user = $this->currentUser();
-    if (\Drupal::currentUser()->hasPermission('custom_permission')) {
-      $userName = \Drupal::currentUser()->getDisplayName();
+    if ($this->currentUser->hasPermission('custom_permission')) {
 
       return [
         '#type' => 'markup',
-        '#markup' => $this->t("Hello") . $user->getDisplayName(),
+        '#markup' => $this->t("Hello") . " " . $user->getDisplayName(),
         '#cache' => [
           'tags' => ['user:' . $this->currentUser()->id()],
         ],
@@ -32,7 +62,7 @@ class ShowTextController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => 'No Permission',
       '#cache' => [
-        'tags' => ['user:' . \Drupal::currentUser()->id()],
+        'tags' => ['user:' . $this->currentUser->id()],
       ],
     ];
   }
