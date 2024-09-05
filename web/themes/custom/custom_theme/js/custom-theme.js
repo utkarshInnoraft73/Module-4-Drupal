922314689de529865b97bf7c1adca64a055515e2
utkarshInnoraft73 <utkarsh.singh@innoraft.com>
2024-09-05 14:50:56 +0530

    FT2-282: Complete the task of the Drupal theming.

---
+++ web/themes/custom/custom_theme/js/custom-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Custom theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.customTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
---
+++ web/themes/custom/sub_bootstrap/js/sub-bootstrap.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * sub-bootstrap behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.subBootstrap = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
